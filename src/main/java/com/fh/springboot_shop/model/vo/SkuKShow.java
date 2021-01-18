9439dd4764e715b582a26be49e1a1ebaf60480df
e <c20002021@163.com>
2021-01-18 20:35:03 +0800

    创建了属性的vo类

---
+++ src/main/java/com/fh/springboot_shop/model/vo/SkuKShow.java
@@ -0,0 +1,76 @@
+package com.fh.springboot_shop.model.vo;
+
+public class SkuKShow {
+
+
+    private  Integer id;
+
+    private  String name;
+
+    private  String nameCH;
+
+    private  String typeName;
+
+    private  Integer typeId;
+
+    private  Integer isSKU;
+
+    private  Integer type;
+
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameCH() {
+        return nameCH;
+    }
+
+    public void setNameCH(String nameCH) {
+        this.nameCH = nameCH;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Integer getIsSKU() {
+        return isSKU;
+    }
+
+    public void setIsSKU(Integer isSKU) {
+        this.isSKU = isSKU;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
